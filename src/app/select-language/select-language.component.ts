5f060f4fb8005b3eca32447d19464444daeaf650
audsostrom <audreyostrom64@gmail.com>
2024-01-24 23:37:01 -0800

    forced language select

--- src/app/select-language/select-language.component.ts
+++ src/app/select-language/select-language.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-language',
   templateUrl: './select-language.component.html',
   styleUrls: ['./select-language.component.scss']
 })
-export class SelectLanguageComponent {
+export class SelectLanguageComponent implements OnInit {
+  selectedValue: number = 0;
+  onChange(event: MatRadioChange) {
+    this.selectedValue = event.value;
+  }
+
+  constructor(private router: Router){
+  }
+
+  redirectPage(){
+    this.router.navigate(['/dashboard']);
+  }
+  ngOnInit() { 
+  
+  }
 
 }
