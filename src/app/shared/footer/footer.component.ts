c2481434e728445bab97ce39388c5b2fee67ca21
audsostrom <audreyostrom64@gmail.com>
2024-01-30 20:23:50 -0800

    added scaffolding for basic information pages

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  // footer is basically that bottom row on pages that takes you to home, map, or profile
+
+}
