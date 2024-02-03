3fd9183d59abf824dbd7f9c7261ac139f78cbb83
audsostrom <audreyostrom64@gmail.com>
2024-02-03 14:51:42 -0800

    resolved merge conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,8 @@ import { LandingComponent } from './landing/landing.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SelectLanguageComponent } from './select-language/select-language.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-<<<<<<< HEAD
-import { EducationComponent } from './education/education.component';
-=======
 import { EducationPageComponent } from './education-page/education-page.component';
->>>>>>> 030eeafc5baf673c4df1b546456fd17e35520dc9
+import { EducationComponent } from './education/education.component';
 
 const routes: Routes = [
   {path: 'landing', component: LandingComponent},
